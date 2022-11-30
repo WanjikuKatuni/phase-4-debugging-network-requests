b386ced36d46b7ee0170a31998cbb67d69da58d8
wanjiku <annritawanjiku77@gmail.com>
2022-11-30 14:16:48 +0300

    complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,22 @@
 class MoviesController < ApplicationController
   
+  # POST /movies
+  def create
+    movie= Movie.create(movie_params)
+    render json:movie, status: :created
+  end
+
+
   def index
     movies = Movie.all
     render json: movies
   end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length,  :description, :poster_url, :category, :discount, :female_director)
+  end
+
 
 end
